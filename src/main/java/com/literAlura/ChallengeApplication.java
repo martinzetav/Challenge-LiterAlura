06c819be053d692e3e011ae64ce46c6a067b62f6
Martin Zalazar <martinzetav@gmail.com>
2025-06-05 08:41:44 -0300

    se implemento interfaz funcional

--- src/main/java/com/literAlura/ChallengeApplication.java
+++ src/main/java/com/literAlura/ChallengeApplication.java
@@ -1,5 +1,8 @@
 package com.literAlura;
 
+import com.literAlura.repository.LibroRepository;
+import com.literAlura.service.Challenge;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChallengeApplication implements CommandLineRunner {
 
+	@Autowired
+	private LibroRepository libroRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("DB_USER: " + System.getenv("USER_DB"));
+		Challenge challenge = new Challenge(libroRepository);
+		challenge.menu();
 	}
 }
