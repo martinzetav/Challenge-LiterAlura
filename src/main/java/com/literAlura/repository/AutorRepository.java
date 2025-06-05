98b9c455cfa75369eb22839959899ffd918e5d76
Martin Zalazar <martinzetav@gmail.com>
2025-06-05 16:27:35 -0300

    se agregaron consultas derivadas

--- src/main/java/com/literAlura/repository/AutorRepository.java
+++ src/main/java/com/literAlura/repository/AutorRepository.java
@@ -3,6 +3,7 @@ package com.literAlura.repository;
 import com.literAlura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,7 @@ public interface AutorRepository extends JpaRepository<Autor, Long> {
     Optional<Autor> findByNombre(String nombre);
     @Query("SELECT a FROM Autor a JOIN FETCH a.libros")
     List<Autor> findAllAutoresConLibros();
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libros WHERE a.fechaNac <= :anio AND (a.fechaFallecimiento IS NULL OR a.fechaFallecimiento >= :anio)")
+    List<Autor> findAutoresVivosConLibros(@Param("anio") String anio);
+
 }
--- src/main/java/com/literAlura/repository/LibroRepository.java
+++ src/main/java/com/literAlura/repository/LibroRepository.java
@@ -4,9 +4,10 @@ import com.literAlura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
     Optional<Libro> findByTitulo(String titulo);
+    List<Libro> findByIdioma(String idioma);
 }
