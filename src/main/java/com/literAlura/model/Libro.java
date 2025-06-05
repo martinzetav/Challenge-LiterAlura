04564587ab89095fa5e122b20807aaa0e7ef6dd0
Martin Zalazar <martinzetav@gmail.com>
2025-06-05 08:37:35 -0300

    se creo la entidad para persistir Libro en db

---
+++ src/main/java/com/literAlura/model/Libro.java
@@ -0,0 +1,25 @@
+package com.literAlura.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "libros")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String titulo;
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Autor> autores;
+    private String idioma;
+    private Double numeroDescargas;
+}
